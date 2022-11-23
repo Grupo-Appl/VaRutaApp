87b97d7468488dca92c2857de43b0e54b73189b4
ClauRozasG <u201922910@upc.edu.pe>
2022-11-23 02:54:33 -0500

    feat: add home

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeComponent from '../shared/public/pages/Home.component.vue'
 
 //const AboutView = () => import('../public/pages/aboutView.vue');
+const Home=() => import('../shared/public/pages/Home.component.vue')
 const Destiny =() => import('../handling/components/destiny-information-section.component.vue')
 const Senders =() => import('../profiles/components/senders-information-section.component.vue')
 const Consignees =() => import('../profiles/components/consignees-information-section.component.vue')
 const Documents =() => import('../handling/components/document-information-section.component.vue')
 const Shipping =() => import('../booking/components/shipping-information-section.component.vue')
+const LogIn =() => import('../security/component/user-consignee.component.vue')
+const Register=() => import('../security/component/user-consignee-register.component.vue')
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: '',
+      component: LogIn
+    },
+    {
       path: '/home',
       name: 'home',
-      component: HomeComponent
-    },
+      component: Home
+    }
+      ,
     {
       path: '/destinations',
       name: 'destinations',
@@ -37,9 +48,14 @@ const router = createRouter({
       component: Documents
     },
     {
-      path: '/envios',
-      name: 'envios',
+      path: '/shipping',
+      name: 'shipping',
       component: Shipping
+    },
+    {
+      path:'/register',
+      name:'register',
+      component: Register
     }
   ]
 })
--- src/shared/public/pages/Home.component.vue
+++ src/shared/public/pages/Home.component.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <h1>This is Home</h1>
+  <div class="textito">
+    <h1>La mejor empresa de carga del Perú</h1>
+    <p>Estamos comprometidos con la seguridad de tu carga y tu tranquilidad</p>
   </div>
+
+  <img src="public/empresa2.png" >
+
 </template>
+
 
 <script >
 console.log('Pagina de inicio');
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@100&display=swap');
+.textito{
+  font-family: 'Kanit', sans-serif;
+  text-align: right;
+  margin-top: 200px;
+  margin-right: 80px;
+}
+p{
+  font-size: 12px;
+  color: black;
+}
+img{
+  width: 50%;
+  height: 50%;
+  margin-left: 100px;
+  margin-top: -230px;
+  border-radius: 5px;
+
+}
+</style>
